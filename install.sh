02e8be3732fe2099060f9399374bfce7de7cec2b
Elkmar <dimitri.jeangerard@gmail.com>
2018-12-24 11:35:16 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+wget https://github.com/bulwark-crypto/Bulwark/releases/download/2.0.0/bulwark-2.0.0.0-linux64.tar.gz
+tar xvzf bulwark-2.0.0.0-linux64.tar.gz
+
+sudo cp bulwark-2.0.0.0/bin/bulwarkd /usr/bin/
+sudo cp bulwark-2.0.0.0/bin/bulwark-cli /usr/bin/
+sudo cp bulwark-2.0.0.0/bin/bulwark-tx /usr/bin/
+
+bulwarkd
+
+nano /home/.bulwark/bulwark.conf
+
+bulwarkd
