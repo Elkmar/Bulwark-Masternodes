b1769c7f6e498985b715479349f273ff60135117
Elkmar <dimitri.jeangerard@gmail.com>
2020-01-27 20:57:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#wget -N https://raw.githubusercontent.com/Elkmar/Bulwark-Masternodes/master/install.sh && chmod -f 777 install.sh && ./install.sh
+
 wget https://github.com/bulwark-crypto/Bulwark/releases/download/2.0.0/bulwark-2.0.0.0-linux64.tar.gz
 tar xvzf bulwark-2.0.0.0-linux64.tar.gz
 
